7ed30587e86d5f3aa949ff84bbaf1deb10ab433f
Simon Kallweit <simon.kallweit@gmail.com>
2023-03-09 23:07:17 +0100

    add clang-format scripts

---
+++ tools/format_code.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set pwd=%~dp0
+set project_dir=%pwd%..\
+set python=%project_dir%tools\python\python.exe
+set clang_format=%project_dir%tools\clang-format\clang-format.exe
+
+pushd %project_dir%
+call %python% %pwd%run_clang_format.py --clang-format-executable=%clang_format% -i -r include src
+popd
