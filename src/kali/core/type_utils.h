4eb4e95c152ec75b32830c6a01a025029b457739
Simon Kallweit <skallweit@nvidia.com>
2023-03-30 17:22:43 +0200

    utils

--- src/kali/core/type_utils.h
+++ src/kali/core/type_utils.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "error.h"
+
+#include <utility>
+
 namespace kali {
 
-template<typename To, typename From>
-To narrow_cast(From from)
+template<typename R, typename T>
+R narrow_cast(T value)
 {
-    // TODO(@skallweit): use std::narrow_cast when it's available or std::in_range
-    return static_cast<To>(from);
+    // TODO(@skallweit): add better error reporting with source_lcation?
+    if (!std::in_range<R>(value))
+        throw Exception("narrow_cast failed");
+    return static_cast<R>(value);
 }
 
 } // namespace kali
