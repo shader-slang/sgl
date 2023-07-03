e1c9bf5bb353bf2df35b6f88e89bcb41b745dc17
Simon Kallweit <simon.kallweit@gmail.com>
2023-07-04 01:15:52 +0200

    kali_test main

--- src/kali/kali_tests.cpp
+++ src/kali/kali_tests.cpp
@@ -1,17 +1,30 @@
 #include "testing.h"
+#include "core/object.h"
+#include "core/logger.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest.h>
 
 int main(int argc, char** argv)
 {
-    (void)argc;
-    (void)argv;
+    KALI_UNUSED(argc);
+    KALI_UNUSED(argv);
+
+    kali::Logger::global().remove_all_outputs();
+    kali::Logger::global().add_debug_console_output();
+    kali::Logger::global().add_file_output("kali_tests.log");
 
     doctest::Context context;
 
-    context.setOption("success", true);
-    context.setOption("-tc", "create_program");
+    // context.setOption("success", true);
+    // context.setOption("-ts", "formats");
 
-    return context.run();
+    int result = context.run();
+
+#if KALI_ENABLE_OBJECT_TRACKING
+    kali::Logger::global().add_console_output();
+    kali::Object::report_alive_objects();
+#endif
+
+    return result;
 }
