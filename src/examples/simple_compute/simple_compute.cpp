3f9a9a48e2892d88a5d892f5b68b9bfdcacc9bd6
Simon Kallweit <skallweit@nvidia.com>
2023-08-02 15:31:13 -0700

    simple compute example

--- src/examples/simple_compute/simple_compute.cpp
+++ src/examples/simple_compute/simple_compute.cpp
@@ -1,11 +1,206 @@
 #include "kali/core/logger.h"
 #include "kali/rhi/device.h"
 #include "kali/rhi/program.h"
+#include "kali/math/vector.h"
+
+#include "shader-cursor.h"
 
 using namespace kali;
 
+
+Slang::ComPtr<slang::ISession> createSlangSession(gfx::IDevice* device)
+{
+    Slang::ComPtr<slang::ISession> slangSession = device->getSlangSession();
+    return slangSession;
+}
+
+Slang::ComPtr<slang::IModule> compileShaderModuleFromFile(slang::ISession* slangSession, char const* filePath)
+{
+    SlangCompileRequest* slangRequest = nullptr;
+    slangSession->createCompileRequest(&slangRequest);
+
+    int translationUnitIndex = spAddTranslationUnit(slangRequest, SLANG_SOURCE_LANGUAGE_SLANG, filePath);
+    spAddTranslationUnitSourceFile(slangRequest, translationUnitIndex, filePath);
+
+    const SlangResult compileRes = spCompile(slangRequest);
+    if (auto diagnostics = spGetDiagnosticOutput(slangRequest)) {
+        printf("%s", diagnostics);
+    }
+
+    if (SLANG_FAILED(compileRes)) {
+        spDestroyCompileRequest(slangRequest);
+        return Slang::ComPtr<slang::IModule>();
+    }
+
+    Slang::ComPtr<slang::IModule> slangModule;
+    spCompileRequest_getModule(slangRequest, translationUnitIndex, slangModule.writeRef());
+    return slangModule;
+}
+
+struct ExampleProgram {
+    int gWindowWidth = 640;
+    int gWindowHeight = 480;
+
+    Slang::ComPtr<gfx::IDevice> gfx_device;
+
+    Slang::ComPtr<slang::ISession> gSlangSession;
+    Slang::ComPtr<slang::IModule> gSlangModule;
+    Slang::ComPtr<gfx::IShaderProgram> gProgram;
+
+    Slang::ComPtr<gfx::IPipelineState> gPipelineState;
+
+    // Slang::Dictionary<int, std::string> gHashedStrings;
+
+    // GPUPrinting gGPUPrinting;
+
+    Slang::ComPtr<gfx::IShaderProgram> loadComputeProgram(slang::IModule* slangModule, char const* entryPointName)
+    {
+        Slang::ComPtr<slang::IEntryPoint> entryPoint;
+        slangModule->findEntryPointByName(entryPointName, entryPoint.writeRef());
+
+        Slang::ComPtr<slang::IComponentType> linkedProgram;
+        Slang::ComPtr<slang::IBlob> diagnostics;
+        entryPoint->link(linkedProgram.writeRef(), diagnostics.writeRef());
+        if (diagnostics) {
+            printf("%s", (char const*)diagnostics->getBufferPointer());
+        }
+
+        // linkedProgram->getLayout();
+        // gGPUPrinting.loadStrings(linkedProgram->getLayout());
+
+        gfx::IShaderProgram::Desc programDesc = {};
+        programDesc.slangGlobalScope = linkedProgram;
+
+        auto shaderProgram = gfx_device->createProgram(programDesc);
+
+        return shaderProgram;
+    }
+
+    Slang::Result execute()
+    {
+        ref<Device> device = Device::create({.type = DeviceType::d3d12, .enable_debug_layers = true});
+        gfx_device = device->get_gfx_device();
+
+        // gfx::IDevice::Desc deviceDesc = {};
+        // deviceDesc.deviceType = gfx::DeviceType::DirectX12;
+        // Slang::Result res = gfxCreateDevice(&deviceDesc, gfx_device.writeRef());
+        // if (SLANG_FAILED(res))
+        //     return res;
+
+
+        gSlangSession = createSlangSession(gfx_device);
+        gSlangModule = compileShaderModuleFromFile(
+            gSlangSession,
+            "c:/projects/kali/src/examples/simple_compute/compute.cs.slang"
+        );
+
+        gProgram = loadComputeProgram(gSlangModule, "main");
+        if (!gProgram)
+            return SLANG_FAIL;
+
+        gfx::ComputePipelineStateDesc desc;
+        desc.program = gProgram;
+        auto pipelineState = gfx_device->createComputePipelineState(desc);
+        if (!pipelineState)
+            return SLANG_FAIL;
+
+        gPipelineState = pipelineState;
+
+        size_t buffer_size = 4 * 1024;
+
+        ref<Buffer> buffer2
+            = device->create_raw_buffer(buffer_size, ResourceUsage::unordered_access, MemoryType::device_local);
+        auto gfx_buffer = buffer2->get_gfx_buffer_resource();
+
+        // gfx::IBufferResource::Desc gfx_buffer_desc = {};
+        // gfx_buffer_desc.type = gfx::IResource::Type::Buffer;
+        // gfx_buffer_desc.sizeInBytes = buffer_size;
+        // gfx_buffer_desc.elementSize = sizeof(float);
+        // gfx_buffer_desc.defaultState = gfx::ResourceState::UnorderedAccess;
+        // gfx_buffer_desc.allowedStates = gfx::ResourceStateSet(
+        //     gfx::ResourceState::CopySource,
+        //     gfx::ResourceState::CopyDestination,
+        //     gfx::ResourceState::UnorderedAccess
+        // );
+        // gfx_buffer_desc.memoryType = gfx::MemoryType::DeviceLocal;
+        // auto gfx_buffer = gfx_device->createBufferResource(gfx_buffer_desc);
+
+        gfx::IResourceView::Desc bufferViewDesc = {};
+        bufferViewDesc.type = gfx::IResourceView::Type::UnorderedAccess;
+        bufferViewDesc.format = gfx::Format::Unknown;
+        auto bufferView = gfx_device->createBufferView(gfx_buffer, nullptr, bufferViewDesc);
+
+        gfx::ITransientResourceHeap::Desc transientResourceHeapDesc = {};
+        transientResourceHeapDesc.constantBufferSize = 256;
+        auto transientHeap = gfx_device->createTransientResourceHeap(transientResourceHeapDesc);
+
+        // gfx::ICommandQueue::Desc queueDesc = {gfx::ICommandQueue::QueueType::Graphics};
+        // auto queue = gfx_device->createCommandQueue(queueDesc);
+        auto queue = device->get_gfx_queue();
+
+        auto commandBuffer = transientHeap->createCommandBuffer();
+        auto encoder = commandBuffer->encodeComputeCommands();
+        auto rootShaderObject = encoder->bindPipeline(gPipelineState);
+        auto cursor = gfx::ShaderCursor(rootShaderObject);
+        cursor["g_buffer"].setResource(bufferView);
+        encoder->dispatchCompute(1024 / 32, 1, 1);
+        encoder->bufferBarrier(gfx_buffer, gfx::ResourceState::UnorderedAccess, gfx::ResourceState::CopySource);
+        encoder->endEncoding();
+        commandBuffer->close();
+        queue->executeCommandBuffer(commandBuffer);
+
+        queue->waitOnHost();
+
+        Slang::ComPtr<ISlangBlob> blob;
+        gfx_device->readBufferResource(gfx_buffer, 0, buffer_size, blob.writeRef());
+
+        const uint32_t* data = (const uint32_t*)blob->getBufferPointer();
+        for (int i = 0; i < 16; ++i) {
+            log_info("data[{}] = {}", i, data[i]);
+        }
+
+        // gGPUPrinting.processGPUPrintCommands(blob->getBufferPointer(), printBufferSize);
+
+        return SLANG_OK;
+    }
+};
+
+struct Test {
+    int a = 2;
+    bool is_valid(int b) const
+    {
+        KALI_ASSERT(a == 1);
+        return a == b;
+    }
+};
+
+
 int main()
 {
+    uint4 a = {1, 2, 3, 4};
+    float3 b = {1, 2, 3};
+    KALI_PRINT(a);
+    KALI_PRINT(b);
+
+    ResourceUsage usage
+        = ResourceUsage::unordered_access | ResourceUsage::acceleration_structure | ResourceUsage(0x345000);
+    KALI_PRINT(usage);
+
+    log_debug("debug message");
+    log_info("info message");
+    log_warn("warning message");
+    log_error("error message");
+    log_fatal("fatal message");
+
+    Test test;
+    test.is_valid(10);
+    KALI_ASSERT(a.x == 2);
+
+    ExampleProgram program;
+    program.execute();
+    return 0;
+
+#if 0
     std::vector<AdapterInfo> adapters = Device::enumerate_adapters();
 
     log_info("Found {} adapters", adapters.size());
@@ -79,4 +274,5 @@ int main()
 #endif
 
     return 0;
+#endif
 }
