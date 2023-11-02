81f013a8144f5bedb7dca624c502b5928b6a16e5
Simon Kallweit <skallweit@nvidia.com>
2023-11-02 14:33:40 +0100

    tweak error reporting from python

--- src/libkali/kali/core/error.cpp
+++ src/libkali/kali/core/error.cpp
@@ -16,19 +16,19 @@ void set_exception_diagnostics(ExceptionDiagnosticFlags flags)
 
 void throw_exception(const std::source_location& loc, std::string_view msg)
 {
-    std::string error_msg
-        = fmt::format("Exception: {}\n{}({}) in function {}", msg, loc.file_name(), loc.line(), loc.function_name());
+    std::string error_msg = fmt::format("Exception: {}\n", msg);
+    error_msg += fmt::format("{}({}) in function {}", loc.file_name(), loc.line(), loc.function_name());
 
     bool debugger_present = is_debugger_present();
+    bool python_active = is_python_active();
+    if (!debugger_present && !python_active)
+        error_msg += "\nStack trace:\n" + format_stacktrace(backtrace(), 10);
 
-    if (is_set(s_exception_diagnostic_flags, ExceptionDiagnosticFlags::log))
+    if (!python_active && is_set(s_exception_diagnostic_flags, ExceptionDiagnosticFlags::log))
         log_fatal(error_msg);
 
     if (is_set(s_exception_diagnostic_flags, ExceptionDiagnosticFlags::break_debugger) && debugger_present)
         debug_break();
-
-    if (!debugger_present)
-        error_msg += "\nStack trace:\n" + format_stacktrace(backtrace(), 10);
 
     throw std::runtime_error(error_msg.c_str());
 }
@@ -36,15 +36,15 @@ void throw_exception(const std::source_location& loc, std::string_view msg)
 void report_assertion(const std::source_location& loc, std::string_view cond)
 {
     std::string error_msg = fmt::format("Assertion failed: {}\n", cond);
-
     error_msg += fmt::format("{}({}) in function {}", loc.file_name(), loc.line(), loc.function_name());
 
     bool debugger_present = is_debugger_present();
-
-    if (!debugger_present)
+    bool python_active = is_python_active();
+    if (!debugger_present && !python_active)
         error_msg += "\nStack trace:\n" + format_stacktrace(backtrace(), 10);
 
-    log_fatal(error_msg);
+    if (!python_active)
+        log_fatal(error_msg);
 
     if (debugger_present)
         debug_break();
