d81c46d30c631298f5048f49f4bd997d16b8658e
Simon Kallweit <skallweit@nvidia.com>
2023-11-27 10:02:56 +0100

    add ShortVector class

---
+++ src/libkali/kali/core/short_vector.h
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <utility>
+#include <algorithm>
+
+namespace kali {
+
+/**
+ * @brief A vector that stores a small number of elements on the stack.
+ *
+ * @tparam T Element type
+ * @tparam N Size of the short vector
+ */
+template<typename T, size_t N>
+class ShortVector {
+public:
+    static_assert(N > 0, "ShortVector must have a size greater than zero.");
+
+    using value_type = T;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using iterator = value_type*;
+    using const_iterator = const value_type*;
+
+    /// Default constructor.
+    ShortVector() noexcept
+        : m_data(m_short_data)
+        , m_size(0)
+        , m_capacity(N)
+    {
+    }
+
+    /// Size constructor.
+    ShortVector(size_type size, const value_type& value)
+        : m_data(m_short_data)
+        , m_size(size)
+        , m_capacity(N)
+    {
+        if (size > m_capacity)
+            grow(size);
+        for (size_type i = 0; i < size; ++i)
+            m_data[i] = value;
+    }
+
+    /// Initializer list constructor.
+    ShortVector(std::initializer_list<value_type> list)
+        : m_data(m_short_data)
+        , m_size(0)
+        , m_capacity(N)
+    {
+        for (const auto& value : list)
+            push_back(value);
+    }
+
+    ~ShortVector()
+    {
+        if (m_data != m_short_data)
+            delete[] m_data;
+    }
+
+    ShortVector(const ShortVector& other) = delete;
+    ShortVector(ShortVector&& other) = delete;
+
+    ShortVector& operator=(const ShortVector& other) = delete;
+    ShortVector& operator=(ShortVector&& other) = delete;
+
+    reference operator[](size_type index) noexcept { return m_data[index]; }
+    const_reference operator[](size_type index) const noexcept { return m_data[index]; }
+
+    reference front() noexcept { return m_data[0]; }
+    const_reference front() const noexcept { return m_data[0]; }
+
+    reference back() noexcept { return m_data[m_size - 1]; }
+    const_reference back() const noexcept { return m_data[m_size - 1]; }
+
+    iterator begin() noexcept { return m_data; }
+    const_iterator begin() const noexcept { return m_data; }
+
+    iterator end() noexcept { return m_data + m_size; }
+    const_iterator end() const noexcept { return m_data + m_size; }
+
+    bool empty() const noexcept { return m_size == 0; }
+
+    value_type* data() noexcept { return m_data; }
+    const value_type* data() const noexcept { return m_data; }
+
+    size_type size() const noexcept { return m_size; }
+    size_type capacity() const noexcept { return m_capacity; }
+
+    void clear() noexcept { m_size = 0; }
+
+    void push_back(const value_type& value)
+    {
+        if (m_size == m_capacity)
+            grow();
+        m_data[m_size++] = value;
+    }
+
+    void push_back(value_type&& value)
+    {
+        if (m_size == m_capacity)
+            grow();
+        m_data[m_size++] = std::move(value);
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args)
+    {
+        if (m_size == m_capacity)
+            grow();
+        m_data[m_size++] = value_type(std::forward<Args>(args)...);
+    }
+
+    void pop_back() { --m_size; }
+
+private:
+    void grow(size_t new_capacity = 0)
+    {
+        m_capacity = new_capacity > m_capacity ? new_capacity : m_capacity * 2;
+        value_type* new_data = new value_type[m_capacity];
+        std::move(m_data, m_data + m_size, new_data);
+        if (m_data != m_short_data)
+            delete[] m_data;
+        m_data = new_data;
+    }
+
+    value_type m_short_data[N];
+    value_type* m_data;
+    size_type m_size;
+    size_type m_capacity;
+};
+
+} // namespace kali
---
+++ src/libkali/kali/core/tests/test_short_vector.cpp
@@ -0,0 +1,117 @@
+#include "testing.h"
+#include "kali/core/short_vector.h"
+
+using namespace kali;
+
+TEST_SUITE_BEGIN("short_vector");
+
+TEST_CASE("default constructor")
+{
+    ShortVector<int, 4> v;
+    CHECK(v.empty());
+    CHECK_EQ(v.size(), 0);
+    CHECK_EQ(v.capacity(), 4);
+    CHECK_EQ(v.begin(), v.end());
+}
+
+TEST_CASE("size constructor")
+{
+    ShortVector<int, 4> v(3, 0);
+    CHECK_EQ(v.size(), 3);
+    CHECK_EQ(v.capacity(), 4);
+    CHECK_EQ(v[0], 0);
+    CHECK_EQ(v[1], 0);
+    CHECK_EQ(v[2], 0);
+}
+
+TEST_CASE("initializer list constructor")
+{
+    ShortVector<int, 4> v1 = {1, 2, 3, 4};
+    CHECK_EQ(v1.size(), 4);
+    CHECK_EQ(v1.capacity(), 4);
+    CHECK_EQ(v1[0], 1);
+    CHECK_EQ(v1[1], 2);
+    CHECK_EQ(v1[2], 3);
+    CHECK_EQ(v1[3], 4);
+
+    ShortVector<int, 4> v2 = {1, 2, 3, 4, 5};
+    CHECK_EQ(v2.size(), 5);
+    CHECK_GT(v2.capacity(), 4);
+    CHECK_EQ(v2[0], 1);
+    CHECK_EQ(v2[1], 2);
+    CHECK_EQ(v2[2], 3);
+    CHECK_EQ(v2[3], 4);
+    CHECK_EQ(v2[4], 5);
+}
+
+TEST_CASE("element access")
+{
+    ShortVector<int, 4> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    CHECK_EQ(v[0], 1);
+    CHECK_EQ(v[1], 2);
+    CHECK_EQ(v[2], 3);
+    CHECK_EQ(v[3], 4);
+    CHECK_EQ(v.front(), 1);
+    CHECK_EQ(v.back(), 4);
+    CHECK_EQ(*v.begin(), 1);
+    CHECK_EQ(*(v.end() - 1), 4);
+    CHECK_EQ(v.data()[0], 1);
+    CHECK_EQ(v.data()[1], 2);
+    CHECK_EQ(v.data()[2], 3);
+    CHECK_EQ(v.data()[3], 4);
+}
+
+TEST_CASE("push_back")
+{
+    ShortVector<int, 4> v;
+    v.push_back(1);
+    CHECK_EQ(v.size(), 1);
+    CHECK_EQ(v.capacity(), 4);
+    CHECK_EQ(v[0], 1);
+    v.push_back(2);
+    CHECK_EQ(v.size(), 2);
+    CHECK_EQ(v.capacity(), 4);
+    CHECK_EQ(v[0], 1);
+    CHECK_EQ(v[1], 2);
+    v.push_back(3);
+    CHECK_EQ(v.size(), 3);
+    CHECK_EQ(v.capacity(), 4);
+    CHECK_EQ(v[0], 1);
+    CHECK_EQ(v[1], 2);
+    CHECK_EQ(v[2], 3);
+    v.push_back(4);
+    CHECK_EQ(v.size(), 4);
+    CHECK_EQ(v.capacity(), 4);
+    CHECK_EQ(v[0], 1);
+    CHECK_EQ(v[1], 2);
+    CHECK_EQ(v[2], 3);
+    CHECK_EQ(v[3], 4);
+    v.push_back(5);
+    CHECK_EQ(v.size(), 5);
+    CHECK_GT(v.capacity(), 4);
+    CHECK_EQ(v[0], 1);
+    CHECK_EQ(v[1], 2);
+    CHECK_EQ(v[2], 3);
+    CHECK_EQ(v[3], 4);
+    CHECK_EQ(v[4], 5);
+}
+
+TEST_CASE("clear")
+{
+    ShortVector<int, 4> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    CHECK_EQ(v.size(), 4);
+    CHECK_EQ(v.capacity(), 4);
+    v.clear();
+    CHECK_EQ(v.size(), 0);
+    CHECK_EQ(v.capacity(), 4);
+}
+
+TEST_SUITE_END();
