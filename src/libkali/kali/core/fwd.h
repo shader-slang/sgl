e81a7f69fbec87d536e646a5dc3b2bb913c3e6b2
Simon Kallweit <skallweit@nvidia.com>
2023-12-01 16:48:36 +0100

    forward declarations

--- src/libkali/kali/core/fwd.h
+++ src/libkali/kali/core/fwd.h
@@ -2,12 +2,74 @@
 
 namespace kali {
 
+// bitmap.h
+
+class Bitmap;
+
+// blob.h
+
+class Blob;
+
+// file_stream.h
+
+class EOFException;
+class FileStream;
+
+// input.h
+
+struct KeyboardEvent;
+struct MouseEvent;
+struct GamepadEvent;
+struct GamepadState;
+
+// logger.h
+
+class LoggerOutput;
+class ConsoleLoggerOutput;
+class FileLoggerOutput;
+class DebugConsoleLoggerOutput;
+class Logger;
+
+// memory_mapped_file_stream.h
+
+class MemoryMappedFileStream;
+
+// memory_mapped_file.h
+
+class MemoryMappedFile;
+
+// memory_stream.h
+
+class MemoryStream;
+
+// object.h
+
 class Object;
 template<typename>
 class ref;
+template<typename>
+class breakable_ref;
 
-class Logger;
+// short_vector.h
+
+template<typename T, size_t N>
+class ShortVector;
+
+// stream.h
+
+class Stream;
+
+// struct.h
+
+class Struct;
+class StructConverter;
+
+// timer.h
+
+class Timer;
+
+// window.h
+
 class Window;
-class Bitmap;
 
 } // namespace kali
