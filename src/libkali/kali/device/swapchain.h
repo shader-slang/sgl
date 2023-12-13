cbb5d112cc88c9a1751a35aacff7650911baee48
Simon Kallweit <simon.kallweit@gmail.com>
2023-12-14 00:36:14 +0100

    swapchain desc default values

--- src/libkali/kali/device/swapchain.h
+++ src/libkali/kali/device/swapchain.h
@@ -14,10 +14,11 @@ namespace kali {
 class Window;
 
 struct SwapchainDesc {
-    Format format;
-    uint32_t width, height;
-    uint32_t image_count;
-    bool enable_vsync;
+    Format format{Format::unknown};
+    uint32_t width{0};
+    uint32_t height{0};
+    uint32_t image_count{3};
+    bool enable_vsync{false};
 };
 
 class KALI_API Swapchain : public Object {
