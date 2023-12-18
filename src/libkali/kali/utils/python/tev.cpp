7a1ca95812d905fb6bf03b86b87001b4eb0d4b08
Simon Kallweit <skallweit@nvidia.com>
2023-12-18 11:36:29 +0100

    remove testing code

--- src/libkali/kali/utils/python/tev.cpp
+++ src/libkali/kali/utils/python/tev.cpp
@@ -4,15 +4,6 @@
 #include "kali/core/bitmap.h"
 #include "kali/device/resource.h"
 
-#include "kali/core/thread.h" // TODO Remove
-
-namespace kali {
-class TestObject : public Object {
-public:
-    std::string value;
-};
-} // namespace kali
-
 KALI_PY_EXPORT(utils_tev)
 {
     using namespace kali;
@@ -62,39 +53,5 @@ KALI_PY_EXPORT(utils_tev)
         "host"_a = "127.0.0.1",
         "port"_a = 14158,
         "max_retries"_a = 3
-    );
-
-    nb::class_<TestObject, Object>(utils, "TestObject").def(nb::init<>()).def_rw("value", &TestObject::value);
-
-    utils.def(
-        "test_async",
-        [](ref<TestObject> object)
-        {
-            static uint64_t counter;
-            uint64_t id = counter++;
-            log_info("schedule task {} ({})", id, object->value);
-
-#if 0
-            TestObject* object_ptr = object;
-            object_ptr->inc_ref();
-            thread::do_async(
-                [id, object_ptr]()
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds((id * 33) % 500));
-                    log_info("task done {} ({})", id, object_ptr->value);
-                    object_ptr->dec_ref();
-                }
-            );
-#else
-            thread::do_async(
-                [id, object]()
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds((id * 33) % 500));
-                    log_info("task done {} ({})", id, object->value);
-                }
-            );
-#endif
-        },
-        nb::call_guard<nb::gil_scoped_release>()
     );
 }
