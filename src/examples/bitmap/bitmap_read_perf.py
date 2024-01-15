3b7d02b20be0707b6f33cd905daa9a2e490f4456
Simon Kallweit <skallweit@nvidia.com>
2024-01-15 11:08:35 +0100

    examples

---
+++ src/examples/bitmap/bitmap_read_perf.py
@@ -0,0 +1,47 @@
+import kali
+import numpy as np
+
+IMAGE_COUNT = 1024
+IMAGE_WIDTH = 1024
+IMAGE_HEIGHT = 1024
+
+
+def write_test():
+    print("writing images")
+    t = kali.Timer()
+    for i in range(IMAGE_COUNT):
+        bmp = kali.Bitmap(
+            np.random.randint(0, 255, (IMAGE_WIDTH, IMAGE_HEIGHT, 3), dtype=np.uint8)
+        )
+        bmp.write_async(f"test{i}.png")
+        # bmp = kali.Bitmap(np.random.rand(IMAGE_WIDTH, IMAGE_HEIGHT, 3, dtype=np.uint8))
+        # bmp.convert(component_type=kali.Bitmap.ComponentType.uint8, srgb_gamma=True).write_async(f"test{i}.exr")
+
+    print("waiting")
+    kali.thread.wait_for_tasks()
+    print("done")
+    print(t.elapsed_s())
+
+
+def read_test_serial():
+    print("reading images (serial)")
+    t = kali.Timer()
+    bmps = []
+    for i in range(IMAGE_COUNT):
+        bmps.append(kali.Bitmap(f"test{i}.png"))
+    print(f"done ({len(bmps)} images)")
+    print(t.elapsed_s())
+
+
+def read_test_parallel():
+    print("reading images (parallel)")
+    t = kali.Timer()
+    paths = list([f"test{i}.png" for i in range(IMAGE_COUNT)])
+    bmps = kali.Bitmap.read_multiple(paths)
+    print(f"done ({len(bmps)} images)")
+    print(t.elapsed_s())
+
+
+# write_test()
+# read_test_serial()
+read_test_parallel()
---
+++ src/examples/tev/tev.py
@@ -0,0 +1,28 @@
+import kali
+from pathlib import Path
+
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+COUNT = 100
+
+device = kali.Device()
+
+tex = device.create_texture(
+    type=kali.TextureType.texture_2d,
+    format=kali.Format.rgba32_float,
+    width=IMAGE_WIDTH,
+    height=IMAGE_HEIGHT,
+    mip_count=1,
+    usage=kali.ResourceUsage.unordered_access,
+)
+
+kernel = device.load_module(
+    Path(__file__).parent / "checkerboard.slang"
+).create_compute_kernel("main")
+
+for i in range(COUNT):
+    kernel.dispatch(
+        thread_count=[tex.width, tex.height, 1],
+        vars={"g_texture": tex, "g_checker_size": i + 1},
+    )
+    kali.utils.show_in_tev_async(tex, f"test_{i:04d}")
