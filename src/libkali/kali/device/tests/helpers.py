86685e4ffd2d4be74f5a511feaf11dd515bed614
Simon Kallweit <skallweit@nvidia.com>
2024-01-16 17:09:24 +0100

    some work on tests

--- src/libkali/kali/device/tests/helpers.py
+++ src/libkali/kali/device/tests/helpers.py
@@ -1,5 +1,7 @@
 import kali
 import sys
+import pytest
+from dataclasses import dataclass
 
 if sys.platform == "win32":
     DEFAULT_DEVICE_TYPES = [kali.DeviceType.d3d12, kali.DeviceType.vulkan]
@@ -9,6 +11,18 @@ elif sys.platform == "darwin":
     DEFAULT_DEVICE_TYPES = [kali.DeviceType.vulkan]
 else:
     raise RuntimeError("Unsupported platform")
+
+ALL_SHADER_MODELS = [
+    kali.ShaderModel.sm_6_0,
+    kali.ShaderModel.sm_6_1,
+    kali.ShaderModel.sm_6_2,
+    kali.ShaderModel.sm_6_3,
+    kali.ShaderModel.sm_6_4,
+    kali.ShaderModel.sm_6_5,
+    kali.ShaderModel.sm_6_6,
+    kali.ShaderModel.sm_6_7,
+]
+
 
 DEVICE_CACHE = {}
 
@@ -20,3 +34,50 @@ def get_device(type: kali.DeviceType, cached: bool = True) -> kali.Device:
         return DEVICE_CACHE[type]
     else:
         return kali.Device(type=type, enable_debug_layers=True)
+
+
+class Context:
+    buffers: dict[str, kali.Buffer]
+
+    def __init__(self):
+        self.buffers = {}
+
+
+def dispatch_compute(
+    device: kali.Device,
+    path: str,
+    entry_point: str,
+    thread_count: list[int],
+    buffers: dict,
+    defines: dict[str, str] = {},
+    shader_model: kali.ShaderModel = kali.ShaderModel.sm_6_6,
+) -> Context:
+    if shader_model > device.supported_shader_model:
+        pytest.skip(f"Shader model {str(shader_model)} not supported")
+
+    # TODO set shader_model
+    kernel = device.load_module(path=path, defines=defines).create_compute_kernel(
+        entry_point
+    )
+
+    ctx = Context()
+    vars = {}
+
+    for name, desc in buffers.items():
+        args = {
+            "struct_type": kernel.reflection[name],
+            "usage": kali.ResourceUsage.shader_resource
+            | kali.ResourceUsage.unordered_access,
+        }
+        if "element_count" in desc:
+            args["element_count"] = desc["element_count"]
+        if "init_data" in desc:
+            args["init_data"] = desc["init_data"]
+
+        buffer = device.create_structured_buffer(**args)
+        ctx.buffers[name] = buffer
+        vars[name] = buffer
+
+    kernel.dispatch(thread_count=thread_count, vars=vars)
+
+    return ctx
--- src/libkali/kali/device/tests/slang/test_float16.py
+++ src/libkali/kali/device/tests/slang/test_float16.py
@@ -9,29 +9,34 @@ import helpers
 
 ELEMENT_COUNT = 1024
 
-
+@pytest.mark.parametrize("view", ["uav", "srv"])
+@pytest.mark.parametrize("shader_model", helpers.ALL_SHADER_MODELS)
 @pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
-def test_cast_float16(device_type):
+def test_float16(device_type, shader_model, view):
     device = helpers.get_device(device_type)
 
-    data = np.random.rand(ELEMENT_COUNT, 2).astype(np.float16)
+    np.random.seed(123)
+    data = np.random.rand(ELEMENT_COUNT).astype(np.float16)
 
     ctx = helpers.dispatch_compute(
         device=device,
-        path=Path(__file__).parent / "test_cast_float16.slang",
+        path=Path(__file__).parent / "test_float16.slang",
         entry_point="main",
+        defines={"USE_UAV": "1" if view == "uav" else "0"},
+        shader_model=shader_model,
         thread_count=[ELEMENT_COUNT, 1, 1],
         buffers={
             "data": {
+                "usage": kali.ResourceUsage.shader_resource
+                | kali.ResourceUsage.unordered_access,
                 "init_data": data,
             },
             "result": {"element_count": ELEMENT_COUNT},
         },
     )
 
-    expected = data.view(np.uint32).flatten()
-    result = ctx.buffers["result"].to_numpy().view(np.uint32).flatten()
-    assert np.all(result == expected)
+    result = ctx.buffers["result"].to_numpy().view(np.float16).flatten()
+    assert np.all(result == data)
 
 
 if __name__ == "__main__":
