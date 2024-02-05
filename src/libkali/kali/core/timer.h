4e582d0e088408b0320ca0c67060003f8f765418
Simon Kallweit <skallweit@nvidia.com>
2024-02-05 15:26:04 +0100

    rdoc timer

--- src/libkali/kali/core/timer.h
+++ src/libkali/kali/core/timer.h
@@ -29,16 +29,16 @@ public:
     /// Elapsed nanoseconds since last reset.
     double elapsed_ns() const { return delta_ns(m_start, now()); }
 
-    /// Elapsed seconds between two time points.
+    /// Compute elapsed seconds between two time points.
     static double delta_s(TimePoint start, TimePoint end) { return (end - start) * 1e-9; }
 
-    /// Elapsed milliseconds between two time points.
+    /// Compute elapsed milliseconds between two time points.
     static double delta_ms(TimePoint start, TimePoint end) { return (end - start) * 1e-6; }
 
-    /// Elapsed microseconds between two time points.
+    /// Compute elapsed microseconds between two time points.
     static double delta_us(TimePoint start, TimePoint end) { return (end - start) * 1e-3; }
 
-    /// Elapsed nanoseconds between two time points.
+    /// Compute elapsed nanoseconds between two time points.
     static double delta_ns(TimePoint start, TimePoint end) { return double(end - start); }
 
     /// Current time point in nanoseconds since epoch.
