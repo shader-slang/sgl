cd589b63f12949a96b2476a90807c6afdba7a85d
Simon Kallweit <skallweit@nvidia.com>
2024-02-08 09:39:40 +0100

    rdoc shader

--- src/libkali/kali/device/python/shader.cpp
+++ src/libkali/kali/device/python/shader.cpp
@@ -11,32 +11,50 @@ KALI_PY_EXPORT(device_shader)
 {
     using namespace kali;
 
-    nb::class_<TypeConformance>(m, "TypeConformance")
-        .def_rw("type_name", &TypeConformance::type_name)
-        .def_rw("interface_name", &TypeConformance::interface_name);
+    nb::class_<TypeConformance>(m, "TypeConformance", D(TypeConformance))
+        .def_rw("type_name", &TypeConformance::type_name, D(TypeConformance, type_name))
+        .def_rw("interface_name", &TypeConformance::interface_name, D(TypeConformance, interface_name));
 
-    nb::bind_map<TypeConformanceList>(m, "TypeConformanceList")
+    nb::bind_map<TypeConformanceList>(m, "TypeConformanceList", D(TypeConformanceList))
         .def(
             "add",
             nb::overload_cast<std::string, std::string, uint32_t>(&TypeConformanceList::add),
             "type_name"_a,
             "interface_name"_a,
-            "id"_a = -1
+            "id"_a = -1,
+            D(TypeConformanceList, add)
         )
         .def(
             "remove",
             nb::overload_cast<std::string, std::string>(&TypeConformanceList::remove),
             "type_name"_a,
-            "interface_name"_a
+            "interface_name"_a,
+            D(TypeConformanceList, remove)
         )
-        .def("add", nb::overload_cast<const TypeConformanceList&>(&TypeConformanceList::add), "other"_a)
-        .def("remove", nb::overload_cast<const TypeConformanceList&>(&TypeConformanceList::remove), "other"_a);
+        .def(
+            "add",
+            nb::overload_cast<const TypeConformanceList&>(&TypeConformanceList::add),
+            "other"_a,
+            D(TypeConformanceList, add_2)
+        )
+        .def(
+            "remove",
+            nb::overload_cast<const TypeConformanceList&>(&TypeConformanceList::remove),
+            "other"_a,
+            D(TypeConformanceList, remove_2)
+        );
 
-    nb::bind_map<DefineList>(m, "DefineList")
-        .def("add", nb::overload_cast<std::string_view, std::string_view>(&DefineList::add), "name"_a, "value"_a = "")
-        .def("remove", nb::overload_cast<std::string_view>(&DefineList::remove), "name"_a)
-        .def("add", nb::overload_cast<const DefineList&>(&DefineList::add), "other"_a)
-        .def("remove", nb::overload_cast<const DefineList&>(&DefineList::remove), "other"_a);
+    nb::bind_map<DefineList>(m, "DefineList", D(DefineList))
+        .def(
+            "add",
+            nb::overload_cast<std::string_view, std::string_view>(&DefineList::add),
+            "name"_a,
+            "value"_a = "",
+            D(DefineList, add)
+        )
+        .def("remove", nb::overload_cast<std::string_view>(&DefineList::remove), "name"_a, D(DefineList, remove))
+        .def("add", nb::overload_cast<const DefineList&>(&DefineList::add), "other"_a, D(DefineList, add_2))
+        .def("remove", nb::overload_cast<const DefineList&>(&DefineList::remove), "other"_a, D(DefineList, remove_2));
 
     nb::kali_enum_flags<SlangCompilerFlags>(m, "SlangCompilerFlags");
 
@@ -57,49 +75,79 @@ KALI_PY_EXPORT(device_shader)
     using kali::SlangSession;
     using kali::SlangEntryPoint;
 
-    nb::class_<SlangSession, Object>(m, "SlangSession")
-        .def_prop_ro("desc", &SlangSession::desc)
-        .def("load_module", &SlangSession::load_module, "path"_a, "defines"_a = DefineList{})
+    nb::class_<SlangSession, Object>(m, "SlangSession", D(SlangSession))
+        .def_prop_ro("desc", &SlangSession::desc, D(SlangSession, desc))
+        .def(
+            "load_module",
+            &SlangSession::load_module,
+            "path"_a,
+            "defines"_a = DefineList{},
+            D(SlangSession, load_module)
+        )
         .def(
             "load_module_from_source",
             &SlangSession::load_module_from_source,
             "source"_a,
             "path"_a = std::filesystem::path{},
             "name"_a = "",
-            "defines"_a = DefineList{}
+            "defines"_a = DefineList{},
+            D(SlangSession, load_module_from_source)
         );
 
-    nb::class_<SlangModule, Object>(m, "SlangModule")
-        .def_prop_ro("global_scope", &SlangModule::global_scope)
-        .def_prop_ro("entry_points", &SlangModule::entry_points)
-        .def("entry_point", &SlangModule::entry_point, "name"_a)
-        .def("create_program", nb::overload_cast<std::string_view>(&SlangModule::create_program), "entry_point_name"_a)
+    nb::class_<SlangModule, Object>(m, "SlangModule", D(SlangModule))
+        .def_prop_ro("global_scope", &SlangModule::global_scope, D(SlangModule, global_scope))
+        .def_prop_ro("entry_points", &SlangModule::entry_points, D(SlangModule, entry_points))
+        .def("entry_point", &SlangModule::entry_point, "name"_a, D(SlangModule, entry_point))
+        .def(
+            "create_program",
+            nb::overload_cast<std::string_view>(&SlangModule::create_program),
+            "entry_point_name"_a,
+            D(SlangModule, create_program)
+        )
         .def(
             "create_program",
             nb::overload_cast<ref<SlangGlobalScope>, ref<SlangEntryPoint>>(&SlangModule::create_program),
             "global_scope"_a,
-            "entry_point"_a
+            "entry_point"_a,
+            D(SlangModule, create_program_2)
         )
         .def(
             "create_program",
             nb::overload_cast<ref<SlangGlobalScope>, std::vector<ref<SlangEntryPoint>>>(&SlangModule::create_program),
             "global_scope"_a,
-            "entry_points"_a
+            "entry_points"_a,
+            D(SlangModule, create_program_3)
         )
-        .def("create_compute_kernel", &SlangModule::create_compute_kernel, "entry_point_name"_a);
+        .def(
+            "create_compute_kernel",
+            &SlangModule::create_compute_kernel,
+            "entry_point_name"_a,
+            D(SlangModule, create_compute_kernel)
+        );
 
-    nb::class_<SlangComponentType, Object>(m, "SlangComponentType");
-    nb::class_<SlangGlobalScope, SlangComponentType>(m, "SlangGlobalScope");
+    nb::class_<SlangComponentType, Object>(m, "SlangComponentType", D(SlangComponentType));
+    nb::class_<SlangGlobalScope, SlangComponentType>(m, "SlangGlobalScope", D(SlangGlobalScope));
 
-    nb::class_<SlangEntryPoint, SlangComponentType>(m, "SlangEntryPoint")
-        .def_prop_ro("name", &SlangEntryPoint::name)
-        .def_prop_ro("stage", &SlangEntryPoint::stage)
-        .def_prop_ro("layout", &SlangEntryPoint::layout, nb::rv_policy::reference_internal)
-        .def("rename", &SlangEntryPoint::rename, "new_name"_a);
+    nb::class_<SlangEntryPoint, SlangComponentType>(m, "SlangEntryPoint", D(SlangEntryPoint))
+        .def_prop_ro("name", &SlangEntryPoint::name, D(SlangEntryPoint, name))
+        .def_prop_ro("stage", &SlangEntryPoint::stage, D(SlangEntryPoint, stage))
+        .def_prop_ro("layout", &SlangEntryPoint::layout, nb::rv_policy::reference_internal, D(SlangEntryPoint, layout))
+        .def("rename", &SlangEntryPoint::rename, "new_name"_a, D(SlangEntryPoint, rename));
 
-    nb::class_<ShaderProgram, Object>(m, "ShaderProgram")
-        .def_prop_ro("program_layout", &ShaderProgram::program_layout, nb::rv_policy::reference_internal)
+    nb::class_<ShaderProgram, Object>(m, "ShaderProgram", D(ShaderProgram))
+        .def_prop_ro(
+            "program_layout",
+            &ShaderProgram::program_layout,
+            nb::rv_policy::reference_internal,
+            D(ShaderProgram, program_layout)
+        )
         // .def_prop_ro("entry_point_layouts", &ShaderProgram::entry_point_layouts)
-        .def("entry_point_layout", &ShaderProgram::entry_point_layout, "index"_a, nb::rv_policy::reference_internal)
-        .def_prop_ro("reflection", &ShaderProgram::reflection);
+        .def(
+            "entry_point_layout",
+            &ShaderProgram::entry_point_layout,
+            "index"_a,
+            nb::rv_policy::reference_internal,
+            D(ShaderProgram, entry_point_layout)
+        )
+        .def_prop_ro("reflection", &ShaderProgram::reflection, D(ShaderProgram, reflection));
 }
