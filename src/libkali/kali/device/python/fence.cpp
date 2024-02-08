56ef82ca28842b860419ceae63bae3e265b70c15
Simon Kallweit <skallweit@nvidia.com>
2024-02-08 09:38:08 +0100

    rdoc fence

--- src/libkali/kali/device/python/fence.cpp
+++ src/libkali/kali/device/python/fence.cpp
@@ -20,12 +20,12 @@ KALI_PY_EXPORT(device_fence)
         .def_rw("shared", &FenceDesc::shared);
     nb::implicitly_convertible<nb::dict, FenceDesc>();
 
-    nb::class_<Fence, DeviceResource>(m, "Fence")
-        .def_prop_ro("desc", &Fence::desc)
-        .def("signal", &Fence::signal, "value"_a = Fence::AUTO)
-        .def("wait", &Fence::wait, "value"_a = Fence::AUTO, "timeout_ns"_a = Fence::TIMEOUT_INFINITE)
-        .def_prop_ro("current_value", &Fence::current_value)
-        .def_prop_ro("signaled_value", &Fence::signaled_value);
+    nb::class_<Fence, DeviceResource>(m, "Fence", D(Fence))
+        .def_prop_ro("desc", &Fence::desc, D(Fence, desc))
+        .def("signal", &Fence::signal, "value"_a = Fence::AUTO, D(Fence, signal))
+        .def("wait", &Fence::wait, "value"_a = Fence::AUTO, "timeout_ns"_a = Fence::TIMEOUT_INFINITE, D(Fence, wait))
+        .def_prop_ro("current_value", &Fence::current_value, D(Fence, current_value))
+        .def_prop_ro("signaled_value", &Fence::signaled_value, D(Fence, signaled_value));
 
     m.attr("Fence").attr("AUTO") = Fence::AUTO;
     m.attr("Fence").attr("TIMEOUT_INFINITE") = Fence::TIMEOUT_INFINITE;
