25b3c1169cc2ab9973124d95e1765b8dce0dcd1e
ccummingsNV <ccummings@nvidia.com>
2024-08-08 15:32:52 +0100

    Mem heap alignment fixes (#57)
    * Fixed or skipped existing tests
    
    * Clean up / add some comments
    
    * Fix a couple more module_from_source issues
    
    * Fix for texture alignment issue + corresponding test
    
    * Fix comment

--- src/sgl/device/tests/test_device.py
+++ src/sgl/device/tests/test_device.py
@@ -3,6 +3,7 @@
 import pytest
 import sgl
 import sys
+import numpy as np
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent))
@@ -25,6 +26,41 @@ def test_create_device(device_type):
     assert device.info.adapter_name != ""
     API_NAMES = {sgl.DeviceType.d3d12: "Direct3D 12", sgl.DeviceType.vulkan: "Vulkan"}
     assert device.info.api_name == API_NAMES[device_type]
+
+
+# Checks fix for alignment issues when creating/accessing a small buffer,
+# followed by creating/accessing a texture.
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_global_buffer_alignment(device_type: sgl.DeviceType):
+    device = helpers.get_device(device_type)
+
+    # Create a small 16B buffer.
+    small_buffer = device.create_buffer(
+        usage=sgl.ResourceUsage.unordered_access | sgl.ResourceUsage.shader_resource,
+        size=16,
+        debug_name="count_buffer",
+        data=np.array([0, 0, 0, 0], dtype=np.uint32),
+    )
+
+    # Read it, resulting in temporary allocation in the device's read back heap of 16B.
+    small_buffer.to_numpy()
+
+    # Data to populate the texture with.
+    texture_data = np.random.rand(256, 256, 3).astype(np.float32).flatten()
+
+    # Create an RGB float texture.
+    texture = device.create_texture(
+        format=sgl.Format.rgb32_float,
+        width=256,
+        height=256,
+        usage=sgl.ResourceUsage.shader_resource,
+        debug_name="render_texture",
+        data=texture_data,
+    )
+
+    # Read it, resulting in temporary allocation in the device's read back heap of 256*256*4*4B.
+    val = texture.to_numpy().astype(np.float32).flatten()
+    assert np.allclose(val, texture_data, atol=1e-6)
 
 
 if __name__ == "__main__":
