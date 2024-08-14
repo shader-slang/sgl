74f3994495c31fb854b76e958ac41566f8ff1aa6
Chris Cummings <ccummings@nvidia.com>
2024-08-14 11:49:34 +0100

    Generating full dictionary and unions for type info

--- tools/extend_pyi.pyi
+++ tools/extend_pyi.pyi
@@ -3,6 +3,44 @@ import libcst as cst
 from pathlib import Path
 from libcst import parse_expression
 
+# List of types we expect to discover that can be constructed from
+# a dictionary. If 'True', a corresponding TypedDict and Union type
+# will be generated. If 'False', it will be ignored. If a type
+# is discovered but not in this list it will be treated as an error.
+DESCRIPTOR_CONVERT_TYPES = {
+    "AppDesc": True,
+    "AppWindowDesc": True,
+    "DeviceDesc": True,
+    "FenceDesc": True,
+    "FramebufferLayoutTargetDesc": True,
+    "FramebufferLayoutDesc": True,
+    "FramebufferDesc": True,
+    "InputElementDesc": True,
+    "VertexStreamDesc": True,
+    "InputLayoutDesc": True,
+    "ComputePipelineDesc": True,
+    "GraphicsPipelineDesc": True,
+    "HitGroupDesc": True,
+    "RayTracingPipelineDesc": True,
+    "QueryPoolDesc": True,
+    "ShaderTableDesc": True,
+    "BufferDesc": True,
+    "TextureDesc": True,
+    "SamplerDesc": True,
+    "SlangCompilerOptions": True,
+    "SlangLinkOptions": True,
+    "SlangSessionDesc": True,
+    "SwapchainDesc": True,
+    "Viewport": True,
+    "ScissorRect": True,
+    "DepthStencilDesc": True,
+    "RasterizerDesc": True,
+    "AspectBlendDesc": True,
+    "TargetBlendDesc": True,
+    "BlendDesc": True,
+    "TextureLoader.Options": True,
+}
+
 code = open(
     Path(__file__).parent / "../build/windows-vs2022/bin/Debug/python/sgl/__init__.pyi"
 ).read()
@@ -28,6 +66,20 @@ def is_setter(node: cst.FunctionDef):
             return True
     return False
 
+def insert_nodes_after_class_by_name(
+    body_nodes: list[cst.CSTNode], class_name: str, nodes_to_insert: list[cst.CSTNode]
+):
+    insert_idx = 0
+    while insert_idx < len(body_nodes):
+        bn = body_nodes[insert_idx]
+        if isinstance(bn, cst.ClassDef) and bn.name.value == class_name:
+            break
+        insert_idx += 1
+    insert_idx += 1
+    for x in nodes_to_insert:
+        body_nodes.insert(insert_idx, x)
+        insert_idx += 1
+
 class FCDFieldInfo:
     def __init__(self, name: cst.Name, annotation: cst.BaseExpression):
         self.name = name
@@ -35,96 +87,16 @@ class FCDFieldInfo:
 
 class FCDStackInfo:
     def __init__(
-        self, porent_class_type: cst.ClassDef | None, class_type: cst.ClassDef
+        self,
+        parent_classs_type: cst.ClassDef | None,
+        class_type: cst.ClassDef,
+        full_name: str,
     ):
-        self.parent_class_type = porent_class_type
+        self.parent_class_type = parent_classs_type
         self.class_type = class_type
         self.valid = False
         self.fields: list[FCDFieldInfo] = []
-
-def build_types_for_descriptor(descriptor: FCDStackInfo):
-
-    # Generate the elements for the dictionary that defines the
-    # new TypedDict. This is really just an element with name:annotation
-    # for each entry, but looks more complex due to insertion of correct
-    # whitespace (each element other than the last adds a newline and
-    # 4 spaces after the comma)
-    dict_elements: list[cst.DictElement] = []
-    for idx in range(len(result.fields)):
-        field = result.fields[idx]
-        if idx < len(result.fields) - 1:
-            dict_elements.append(
-                cst.DictElement(
-                    key=cst.SimpleString(f'"{field.name.value}"'),
-                    value=field.annotation,
-                    comma=cst.Comma(
-                        whitespace_after=cst.ParenthesizedWhitespace(
-                            last_line=cst.SimpleWhitespace("    ")
-                        )
-                    ),
-                )
-            )
-        else:
-            dict_elements.append(
-                cst.DictElement(
-                    key=cst.SimpleString(f'"{field.name.value}"'),
-                    value=field.annotation,
-                )
-            )
-
-    # Generate the dictionary definition. Formatting adds a new line
-    # and 4 spaces after the opening brace and a new line before the closing
-    # brace.
-    dict_def = cst.Dict(
-        elements=dict_elements,
-        lbrace=cst.LeftCurlyBrace(
-            whitespace_after=cst.ParenthesizedWhitespace(
-                last_line=cst.SimpleWhitespace("    ")
-            )
-        ),
-        rbrace=cst.RightCurlyBrace(whitespace_before=cst.ParenthesizedWhitespace()),
-    )
-
-    # Wrap the dictionary in a call to 'TypedDict' to create the new type
-    typed_dict_annotation = cst.Call(
-        func=cst.Name("TypedDict"),
-        args=[
-            cst.Arg(
-                value=cst.SimpleString(f'"{result.class_type.name.value}Dict"'),
-            ),
-            cst.Arg(value=dict_def),
-        ],
-    )
-
-    # Store the new TypedDict in a type alias
-    type_alias = cst.Assign(
-        targets=[
-            cst.AssignTarget(target=cst.Name(f"{result.class_type.name.value}Dict"))
-        ],
-        value=typed_dict_annotation,
-    )
-
-    # Also generate a corresponding union that combines the source type with the new TypedDict
-    union_type_alias = cst.Assign(
-        targets=[
-            cst.AssignTarget(target=cst.Name(f"{result.class_type.name.value}Type"))
-        ],
-        value=cst.Subscript(
-            value=cst.Name("Union"),
-            slice=[
-                cst.SubscriptElement(
-                    slice=cst.Index(value=cst.Name(result.class_type.name.value))
-                ),
-                cst.SubscriptElement(
-                    slice=cst.Index(
-                        value=cst.Name(f"{result.class_type.name.value}Dict")
-                    )
-                ),
-            ],
-        ),
-    )
-
-    return type_alias, union_type_alias
+        self.full_name = full_name
 
 class FindConvertableToDictionaryTypes(cst.CSTVisitor):
     def __init__(self):
@@ -132,13 +104,16 @@ class FindConvertableToDictionaryTypes(cst.CSTVisitor):
         self.stack: list[FCDStackInfo] = []
         self.results: list[FCDStackInfo] = []
         self.read_class = False
+        self.name_stack: list[str] = []
 
     def visit_ClassDef(self, node: cst.ClassDef) -> Optional[bool]:
         parent = self.stack[-1].class_type if len(self.stack) > 0 else None
-        self.stack.append(FCDStackInfo(parent, node))
+        self.name_stack.append(node.name.value)
+        self.stack.append(FCDStackInfo(parent, node, ".".join(self.name_stack)))
 
     def leave_ClassDef(self, original_node: cst.ClassDef) -> None:  # type: ignore
         self.stack.pop()
+        self.name_stack.pop()
 
     def visit_FunctionDef(self, node: cst.FunctionDef) -> Optional[bool]:
         if node.name.value == "__init__":
@@ -176,185 +151,172 @@ class FindConvertableToDictionaryTypes(cst.CSTVisitor):
                     )
                 )
 
-class TypingTransformer(cst.CSTTransformer):
+class InsertTypesTransformer(cst.CSTTransformer):
     def __init__(self, discovered_descriptor_types: list[FCDStackInfo]):
         super().__init__()
-        self.stack: list[cst.ClassDef] = []
         self.discovered_descriptor_types = discovered_descriptor_types
-
-    def visit_Module(self, node: cst.Module) -> Optional[bool]:
-        return True
-
+    # On leaving a module, insert the dictionary and union types for any global descriptor types
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
     ) -> cst.Module:
-        new_types: list[cst.CSTNode] = []
-        for desc_type in self.discovered_descriptor_types:
-            if desc_type.parent_class_type is None:
-                dict_type, union_type = build_types_for_descriptor(desc_type)
-                new_types.append(dict_type)
-                new_types.append(cst.Newline())
-                new_types.append(union_type)
-                new_types.append(cst.Newline())
-
-        if len(new_types) > 0:
-            insert_idx = 0
-            while insert_idx < len(original_node.body):
-                if isinstance(original_node.body[insert_idx], cst.ClassDef):
-                    break
-                insert_idx += 1
-            new_body = (
-                list(original_node.body[:insert_idx])
-                + new_types
-                + list(original_node.body[insert_idx:])
-            )
-            return updated_node.with_changes(body=new_types)
+        new_body: list[cst.CSTNode] = list(updated_node.body)
+        changed = self._insert_descriptor_nodes(None, new_body)
+        if changed:
+            return updated_node.with_changes(body=new_body)
         return updated_node
-
-    def visit_ClassDef(self, node: cst.ClassDef) -> Optional[bool]:
-        self.stack.append(node)
-
+    # On leaving a class, insert the dictionary and union types for any child descriptor types
     def leave_ClassDef(  # type: ignore
         self, original_node: cst.ClassDef, updated_node: cst.ClassDef
     ) -> cst.CSTNode:
-        self.stack.pop()
-        new_types: list[cst.CSTNode] = []
-        for desc_type in self.discovered_descriptor_types:
-            if desc_type.parent_class_type == original_node:
-                pass
-        if len(new_types) > 0:
-            class_body = original_node.body
-            class_body = class_body.with_changes(body=new_types + list(class_body.body))
+        new_body: list[cst.CSTNode] = list(updated_node.body.body)
+        changed = self._insert_descriptor_nodes(original_node, new_body)
+        if changed:
+            class_body = updated_node.body
+            class_body = class_body.with_changes(body=new_body)
             return updated_node.with_changes(body=class_body)
 
         return updated_node
+    # Finds any descriptor classes that are children of the specified parent (or None for module)
+    # and inserts the dictionary and union types after the corresponding class definition.
+    def _insert_descriptor_nodes(
+        self, original_parent: cst.ClassDef | None, updated_body: list[cst.CSTNode]
+    ) -> bool:
+        changed = False
+        for desc_type in self.discovered_descriptor_types:
+            if desc_type.parent_class_type == original_parent:
+                dict_type, union_type = self._build_types_for_descriptor(desc_type)
+                insert_nodes_after_class_by_name(
+                    updated_body,
+                    desc_type.class_type.name.value,
+                    [cst.Newline(), dict_type, cst.Newline(), union_type],
+                )
+                changed = True
+        return changed
+    # Generates the dictionary and union types for a descriptor class.
+    def _build_types_for_descriptor(self, result: FCDStackInfo):
+
+        # Generate the elements for the dictionary that defines the
+        # new TypedDict. This is really just an element with name:annotation
+        # for each entry, but looks more complex due to insertion of correct
+        # whitespace (each element other than the last adds a newline and
+        # 4 spaces after the comma)
+        dict_elements: list[cst.DictElement] = []
+        for idx in range(len(result.fields)):
+            field = result.fields[idx]
+            if idx < len(result.fields) - 1:
+                dict_elements.append(
+                    cst.DictElement(
+                        key=cst.SimpleString(f'"{field.name.value}"'),
+                        value=field.annotation,
+                        comma=cst.Comma(
+                            whitespace_after=cst.ParenthesizedWhitespace(
+                                last_line=cst.SimpleWhitespace("    "), indent=True
+                            )
+                        ),
+                    )
+                )
+            else:
+                dict_elements.append(
+                    cst.DictElement(
+                        key=cst.SimpleString(f'"{field.name.value}"'),
+                        value=field.annotation,
+                    )
+                )
+
+        # Generate the dictionary definition. Formatting adds a new line
+        # and 4 spaces after the opening brace and a new line before the closing
+        # brace.
+        dict_def = cst.Dict(
+            elements=dict_elements,
+            lbrace=cst.LeftCurlyBrace(
+                whitespace_after=cst.ParenthesizedWhitespace(
+                    last_line=cst.SimpleWhitespace("    "), indent=True
+                )
+            ),
+            rbrace=cst.RightCurlyBrace(
+                whitespace_before=cst.ParenthesizedWhitespace(indent=True)
+            ),
+        )
+
+        # Wrap the dictionary in a call to 'TypedDict' to create the new type
+        typed_dict_annotation = cst.Call(
+            func=cst.Name("TypedDict"),
+            args=[
+                cst.Arg(
+                    value=cst.SimpleString(f'"{result.class_type.name.value}Dict"'),
+                ),
+                cst.Arg(value=dict_def),
+            ],
+        )
+
+        # Store the new TypedDict in a type alias
+        type_alias = cst.SimpleStatementLine(
+            body=[
+                cst.Assign(
+                    targets=[
+                        cst.AssignTarget(
+                            target=cst.Name(f"{result.class_type.name.value}Dict")
+                        )
+                    ],
+                    value=typed_dict_annotation,
+                )
+            ]
+        )
+
+        # Also generate a corresponding union that combines the source type with the new TypedDict
+        union_type_alias = cst.SimpleStatementLine(
+            body=[
+                cst.Assign(
+                    targets=[
+                        cst.AssignTarget(
+                            target=cst.Name(f"{result.class_type.name.value}Type")
+                        )
+                    ],
+                    value=cst.Subscript(
+                        value=cst.Name("Union"),
+                        slice=[
+                            cst.SubscriptElement(
+                                slice=cst.Index(
+                                    value=cst.Name(result.class_type.name.value)
+                                )
+                            ),
+                            cst.SubscriptElement(
+                                slice=cst.Index(
+                                    value=cst.Name(
+                                        f"{result.class_type.name.value}Dict"
+                                    )
+                                )
+                            ),
+                        ],
+                    ),
+                )
+            ]
+        )
+
+        return type_alias, union_type_alias
 
 findconv = FindConvertableToDictionaryTypes()
 stub_tree.visit(findconv)
 
-transformer = TypingTransformer(findconv.results)
-
-stub_tree.visit(transformer)
-
-new_types: list[cst.CSTNode] = []
-
 for result in findconv.results:
+    if not result.full_name in DESCRIPTOR_CONVERT_TYPES:
+        raise Exception(
+            f"Discovered descriptor class {result.full_name}, but not in list of types to convert."
+        )
+for conv_type in DESCRIPTOR_CONVERT_TYPES:
+    if not any(result.full_name == conv_type for result in findconv.results):
+        raise Exception(
+            f"Type {conv_type} is in list of types to convert but no corresponding descriptor class was discovered."
+        )
 
-    dict_elements: list[cst.DictElement] = []
-    for idx in range(len(result.fields)):
-        field = result.fields[idx]
-        if idx < len(result.fields) - 1:
-            dict_elements.append(
-                cst.DictElement(
-                    key=cst.SimpleString(f'"{field.name.value}"'),
-                    value=field.annotation,
-                    comma=cst.Comma(
-                        whitespace_after=cst.ParenthesizedWhitespace(
-                            last_line=cst.SimpleWhitespace("    ")
-                        )
-                    ),
-                )
-            )
-        else:
-            dict_elements.append(
-                cst.DictElement(
-                    key=cst.SimpleString(f'"{field.name.value}"'),
-                    value=field.annotation,
-                )
-            )
+transformer = InsertTypesTransformer(findconv.results)
 
-    dict_def = cst.Dict(
-        elements=dict_elements,
-        lbrace=cst.LeftCurlyBrace(
-            whitespace_after=cst.ParenthesizedWhitespace(
-                last_line=cst.SimpleWhitespace("    ")
-            )
-        ),
-        rbrace=cst.RightCurlyBrace(whitespace_before=cst.ParenthesizedWhitespace()),
-    )
-
-    typed_dict_annotation = cst.Call(
-        func=cst.Name("TypedDict"),
-        args=[
-            cst.Arg(
-                value=cst.SimpleString(f'"{result.class_type.name.value}Dict"'),
-            ),
-            cst.Arg(value=dict_def),
-        ],
-    )
-
-    type_alias = cst.Assign(
-        targets=[
-            cst.AssignTarget(target=cst.Name(f"{result.class_type.name.value}Dict"))
-        ],
-        value=typed_dict_annotation,
-    )
-    new_types.append(type_alias)
-    new_types.append(cst.Newline())
-
-    print(stub_tree.code_for_node(type_alias))
-
-    x = Union[int, float]
-
-    union_type_alias = cst.Assign(
-        targets=[
-            cst.AssignTarget(target=cst.Name(f"{result.class_type.name.value}Type"))
-        ],
-        value=cst.Subscript(
-            value=cst.Name("Union"),
-            slice=[
-                cst.SubscriptElement(
-                    slice=cst.Index(value=cst.Name(result.class_type.name.value))
-                ),
-                cst.SubscriptElement(
-                    slice=cst.Index(
-                        value=cst.Name(f"{result.class_type.name.value}Dict")
-                    )
-                ),
-            ],
-        ),
-    )
-    new_types.append(union_type_alias)
-    new_types.append(cst.Newline())
-
-    print(stub_tree.code_for_node(union_type_alias))
-
-    pass
-
-insert_idx = 0
-while insert_idx < len(stub_tree.body):
-    if isinstance(stub_tree.body[insert_idx], cst.ClassDef):
-        break
-    insert_idx += 1
-
-new_imports = [
-    cst.SimpleStatementLine(
-        body=[
-            cst.ImportFrom(
-                module=cst.Name("typing"),
-                names=[
-                    cst.ImportAlias(name=cst.Name("TypedDict"), asname=None),
-                    cst.ImportAlias(name=cst.Name("Union"), asname=None),
-                ],
-                whitespace_after_import=cst.SimpleWhitespace("    "),
-            )
-        ]
-    )
-]
-
-new_body = (
-    list(stub_tree.body[:insert_idx])
-    + new_imports
-    + new_types
-    + list(stub_tree.body[insert_idx:])
-)
-
-new_tree = stub_tree.with_changes(body=new_body)
+new_tree = stub_tree.visit(transformer)
 
 open(
     Path(__file__).parent
     / "../build/windows-vs2022/bin/Debug/python/sgl/__init__2.pyi",
     "w",
-).write(stub_tree.code_for_node(stub_tree.with_changes(body=new_body)))
+).write(new_tree.code)
 
 pass
