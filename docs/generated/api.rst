0f6839c3c0b9cf9c53f345a044686610ee3f8254
ccummingsNV <ccummings@nvidia.com>
2025-01-06 11:25:20 +0000

    Version 0.4.0 (#146)
    * Update changlog
    
    * Update version numbers

--- docs/generated/api.rst
+++ docs/generated/api.rst
@@ -576,7 +576,7 @@ Constants
 
 .. py:data:: sgl.SGL_VERSION
     :type: str
-    :value: "0.3.0"
+    :value: "0.4.0"
 
 
 
@@ -592,7 +592,7 @@ Constants
 
 .. py:data:: sgl.SGL_VERSION_MINOR
     :type: int
-    :value: 3
+    :value: 4
 
 
 
--- src/sgl/sgl.h
+++ src/sgl/sgl.h
@@ -5,7 +5,7 @@
 #include "sgl/core/macros.h"
 
 #define SGL_VERSION_MAJOR 0
-#define SGL_VERSION_MINOR 3
+#define SGL_VERSION_MINOR 4
 #define SGL_VERSION_PATCH 0
 
 #define SGL_VERSION                                                                                                    \
