8dc896e2881b65f1957db19223b2b7d8856b3c30
kaizhang <kazhang@nvidia.com>
2025-03-11 12:56:27 -0500

    update the test

--- src/sgl/device/tests/test_parameterBlock.py
+++ src/sgl/device/tests/test_parameterBlock.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 import numpy as np
 import sgl
@@ -13,6 +15,11 @@ import sglhelpers as helpers
 # to test the ParameterBlock binding.
 @pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
 def test_parameter_block(device_type: sgl.DeviceType):
+    
+    # Skip this test on Metal devices
+    if device_type == sgl.DeviceType.metal:
+        pytest.skip("Skipping parameter block test until https://github.com/shader-slang/slang/pull/6577 is merged")
+    
     # Create device
     print(f"Testing {device_type}")
 
@@ -61,11 +68,13 @@ def test_parameter_block(device_type: sgl.DeviceType):
         encoder.dispatch(thread_count=[1, 1, 1])
     
     # Submit the command buffer
-    fenceValue = command_buffer.submit()
-    device.wait_command_buffer(fenceValue)
+    command_buffer.submit()
     
     # Read back the result
     result = output_buffer.to_numpy().view(np.float32)[0]
     
     # Verify the result
-    assert result == 21.0, f"Expected 6.0, got {result}"
+    assert result == 21.0, f"Expected 21.0, got {result}"
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v", "-s"])
